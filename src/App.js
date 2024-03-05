b19cd0093df27acf497c9c223879c34f022a5b88
AndrewTurinawe <turinskandrew69@gmail.com>
2024-03-05 10:25:36 +0300

    Initial-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Weatherapp from "./components/weather-app/Weatherapp";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weatherapp/>
+    </div>
+  );
+}
+
+export default App;
